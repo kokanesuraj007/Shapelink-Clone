9aa0f00e879b04926394006ad776736b8b91cc99
kokanesuraj007 <surajkokane1997@gmail.com>
2022-09-28 18:35:40 +0530

    navbar

--- my-app/src/Components/Navbar.jsx
+++ my-app/src/Components/Navbar.jsx
@@ -1,7 +1,47 @@
 import React from 'react'
+import { Container,Box, Image,Button, Flex, Heading, Spacer } from "@chakra-ui/react"
 
-export const Navbar = () => {
+const Navbar = () => {
+
+
   return (
-    <div>Navbar</div>
+    <Container>
+      <Box size="sm">
+        <Image 
+        src="https://www.shapelink.com/images/startpage.jpg" 
+        alt="Segun Adebayo"
+        size="50px"
+        width="100%"
+         zIndex="1"
+        
+         objectFit="cover"
+         background=" #000 " 
+        //  no-repeat= "center center"
+          background-size= "cover"
+          background-attachment= "fixed"
+          padding= "75px 0px 50px "
+            // z-index: 1;
+        />
+        <Flex className='fx' py="0.5" my={-1180} padding="50px" bg="null" >
+        <Heading>
+        <Image src='https://kcdn.twiikapp.com/images/logo.png' height="40px" paddingLeft="-100px"/>
+        
+        </Heading>
+        <Spacer />
+        <Button  mx={25} border="none" bg="">Get the app</Button>
+        <Button  mx={25} border="none" bg="">Contact</Button>
+        <Button  mx={25} border="none" bg="">LOGIN</Button>
+        <Button  mx={25} border="none" bg="">Create account</Button>
+      </Flex>
+      </Box>
+
+
+    </Container>
+
+
+
+    
   )
 }
+
+export default Navbar
