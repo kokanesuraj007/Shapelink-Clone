550c656a51236229ed6a961fb2a02c4125801341
suraj sanjay kokane <108039887+kokanesuraj007@users.noreply.github.com>
2022-09-29 22:34:45 +0530

    d3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './Pages/Home';
+import LandingPage from './Pages/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+       <Navbar/>
       <Home/>
+      <LandingPage/>
     </div>
   );
 }
