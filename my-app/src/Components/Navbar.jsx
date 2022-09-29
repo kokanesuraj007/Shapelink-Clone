af09e38e70a1eee7571183aaae072eecc452057d
kokanesuraj007 <surajkokane1997@gmail.com>
2022-09-29 20:44:52 +0530

    day-2

--- my-app/src/Components/Navbar.jsx
+++ my-app/src/Components/Navbar.jsx
@@ -1,42 +1,37 @@
 import React from 'react'
-import { Container,Box, Image,Button, Flex, Heading, Spacer } from "@chakra-ui/react"
-
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa"
+import "../Styles/main.css";
 const Navbar = () => {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
 
 
   return (
-    <Container>
-      <Box size="sm">
-        <Image 
-        src="https://www.shapelink.com/images/startpage.jpg" 
-        alt="Segun Adebayo"
-        size="50px"
-        width="100%"
-         zIndex="1"
-        
-         objectFit="cover"
-         background=" #000 " 
-        //  no-repeat= "center center"
-          background-size= "cover"
-          background-attachment= "fixed"
-          padding= "75px 0px 50px "
-            // z-index: 1;
-        />
-        <Flex className='fx' py="0.5" my={-1180} padding="50px" bg="null" >
-        <Heading>
-        <Image src='https://kcdn.twiikapp.com/images/logo.png' height="40px" paddingLeft="-100px"/>
-        
-        </Heading>
-        <Spacer />
-        <Button  mx={25} border="none" bg="">Get the app</Button>
-        <Button  mx={25} border="none" bg="">Contact</Button>
-        <Button  mx={25} border="none" bg="">LOGIN</Button>
-        <Button  mx={25} border="none" bg="">Create account</Button>
-      </Flex>
-      </Box>
+ 
+    <header>
+    <div>
+      <img src="https://kcdn.twiikapp.com/images/logo.png" alt="logo" />
+    </div>
+
+    <nav ref={navRef}>
+      <a href="/#">Get the app</a>
+      <a href="/#">Contact</a>
+      <a href="/#">Login</a>
+      <a href="/#">Create account</a>
+      <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+        <FaTimes />
+      </button>
+    </nav>
+    <button className="nav-btn" onClick={showNavbar}>
+      <FaBars />
+    </button>
+  </header>
 
 
-    </Container>
 
 
 
